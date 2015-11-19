1b225006ca830348d71931522d1220baf81808bb
Karrmarr <karrmarr@doesnotwork.com>
2015-11-19 21:05:47 +0100

    Inserted the buttons on the homepage

--- MonthlyBalance/HomeViewController.swift
+++ MonthlyBalance/HomeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UIPageViewControllerDataSource {
+class HomeViewController: UIViewController {
 
   @IBOutlet weak var balanceInfoContainerView: UIView!
   
@@ -37,27 +37,6 @@ class HomeViewController: UIViewController, UIPageViewControllerDataSource {
     // initialize Page View Controller
     initializePageViewController()
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-  // MARK: - Helper methods for PageViewController
-  
-  func balanceInfoViewControllerAtIndex(index: Int) -> BalanceInfoViewController? {
-    if index < 0 || index >= self.balanceTitles.count {
-      return nil
-    }
-    
-    let viewController = BalanceInfoViewController()
-    let view = viewController.view as! BalanceInfoView
-    
-    view.headline = self.balanceTitles[index]
-    viewController.pageIndex = index
-    
-    return viewController
-  }
   
   override func updateViewConstraints() {
     self.gradientLayer.frame = self.gradientBackgroundView.layer.bounds
@@ -65,62 +44,8 @@ class HomeViewController: UIViewController, UIPageViewControllerDataSource {
     super.updateViewConstraints()
   }
   
-  // MARK: - PageViewController DataSource
-  
-  func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
-    return self.balanceTitles.count
-  }
-  
-  func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
-    return 0
-  }
-  
-  func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
-    let balanceInfoViewController = viewController as! BalanceInfoViewController
-    var index = balanceInfoViewController.pageIndex
-    
-    if index == 0 || index == NSNotFound {
-      return nil
-    }
-    
-    index--
-    return self.balanceInfoViewControllerAtIndex(index)
-  }
-  
-  func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
-    let balanceInfoViewController = viewController as! BalanceInfoViewController
-    var index = balanceInfoViewController.pageIndex
-    
-    if index == (self.balanceTitles.count - 1) || index == NSNotFound {
-      return nil
-    }
-    
-    index++
-    return self.balanceInfoViewControllerAtIndex(index)
-  }
-    
   // MARK: - Private methods
-  
-  private func initializePageViewController() {
-    // Initialize page view controller
-    let pageViewController = UIPageViewController(transitionStyle: .Scroll, navigationOrientation: UIPageViewControllerNavigationOrientation.Horizontal, options: [:])
-    pageViewController.view.backgroundColor = UIColor(hex: kBaseBackgroundColor)
-    pageViewController.dataSource = self
     
-    let startPage = self.balanceInfoViewControllerAtIndex(0)
-    pageViewController.setViewControllers([startPage!], direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
-    
-    // add page view controller as a child view controller
-    self.addChildViewController(pageViewController)
-    
-    pageViewController.view.frame = CGRect(x: 0, y: 0, width: self.balanceInfoContainerView.bounds.size.width, height: self.balanceInfoContainerView.bounds.size.height)
-    self.balanceInfoContainerView.addSubview(pageViewController.view)
-
-    pageViewController.didMoveToParentViewController(self)
-    
-    self.balancePageViewController = pageViewController
-  }
-  
   private func gradientBackgroundLayer(size: CGSize) -> CAGradientLayer {
     let colors = [ UIColor(hex: kGradientBackgroundColor1).CGColor, UIColor(hex: kGradientBackgroundColor2).CGColor ]
     let locations = [ 0.0, 1.0 ]
---
+++ MonthlyBalance/HomeViewControllerPageViewExtension.swift
@@ -0,0 +1,82 @@
+//
+//  HomeViewControllerPageViewExtension.swift
+//  MonthlyBalance
+//
+//  Created by Thorsten Klusemann on 19.11.15.
+//  Copyright © 2015 Karrmarr Software. All rights reserved.
+//
+
+import UIKit
+
+extension HomeViewController : UIPageViewControllerDataSource {
+  
+  // MARK: - PageViewController DataSource
+  
+  func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+    return self.balanceTitles.count
+  }
+  
+  func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+    return 0
+  }
+  
+  func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+    let balanceInfoViewController = viewController as! BalanceInfoViewController
+    var index = balanceInfoViewController.pageIndex
+    
+    if index == 0 || index == NSNotFound {
+      return nil
+    }
+    
+    index--
+    return self.balanceInfoViewControllerAtIndex(index)
+  }
+  
+  func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+    let balanceInfoViewController = viewController as! BalanceInfoViewController
+    var index = balanceInfoViewController.pageIndex
+    
+    if index == (self.balanceTitles.count - 1) || index == NSNotFound {
+      return nil
+    }
+    
+    index++
+    return self.balanceInfoViewControllerAtIndex(index)
+  }
+  
+  // MARK: - Helper methods for PageViewController
+  
+  func balanceInfoViewControllerAtIndex(index: Int) -> BalanceInfoViewController? {
+    if index < 0 || index >= self.balanceTitles.count {
+      return nil
+    }
+    
+    let viewController = BalanceInfoViewController()
+    let view = viewController.view as! BalanceInfoView
+    
+    view.headline = self.balanceTitles[index]
+    viewController.pageIndex = index
+    
+    return viewController
+  }
+
+  func initializePageViewController() {
+    // Initialize page view controller
+    let pageViewController = UIPageViewController(transitionStyle: .Scroll, navigationOrientation: UIPageViewControllerNavigationOrientation.Horizontal, options: [:])
+    pageViewController.view.backgroundColor = UIColor(hex: kBaseBackgroundColor)
+    pageViewController.dataSource = self
+    
+    let startPage = self.balanceInfoViewControllerAtIndex(0)
+    pageViewController.setViewControllers([startPage!], direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
+    
+    // add page view controller as a child view controller
+    self.addChildViewController(pageViewController)
+    
+    pageViewController.view.frame = CGRect(x: 0, y: 0, width: self.balanceInfoContainerView.bounds.size.width, height: self.balanceInfoContainerView.bounds.size.height)
+    self.balanceInfoContainerView.addSubview(pageViewController.view)
+    
+    pageViewController.didMoveToParentViewController(self)
+    
+    self.balancePageViewController = pageViewController
+  }
+}
